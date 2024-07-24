d51fc339b03e81fa4fb36c57f921d37bf413848d
Merdekasoft <merdekasoft@gmail.com>
2024-07-24 09:07:21 +0700

    Update remaster.sh

--- remaster.sh
+++ remaster.sh
@@ -1,8 +1,9 @@
+
 sudo apt-get update
 sudo apt-get install live-build
 mkdir live-build-project
 cd live-build-project
-lb config --distribution bookworm --debian-installer live
+lb config --distribution bookworm --debian-installer none
 mkdir -p config/package-lists
 echo "i3" > config/package-lists/my.list.chroot
 echo "lightdm" >> config/package-lists/my.list.chroot
@@ -12,6 +13,11 @@ echo "git" >> config/package-lists/my.list.chroot
 echo "mc" >> config/package-lists/my.list.chroot
 echo "htop" >> config/package-lists/my.list.chroot
 echo "slick-greeter" >> config/package-lists/my.list.chroot
+echo "calamares" >> config/package-lists/my.list.chroot
+echo "calamares-settings-debian" >> config/package-lists/my.list.chroot
+echo "rxvt-unicode" >> config/package-lists/my.list.chroot
+echo "rxvt-unicode-256color" >> config/package-lists/my.list.chroot
+echo "rxvt-unicode-terminfo" >> config/package-lists/my.list.chroot
 
 mkdir -p config/hooks/live
 touch config/hooks/live/99-custom-script.chroot
