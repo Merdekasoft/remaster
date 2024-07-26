7330fcee77aab570628c181af8bd0c7a7d7136b2
Merdekasoft <merdekasoft@gmail.com>
2024-07-27 06:24:17 +0700

    Update remaster.sh

--- remaster.sh
+++ remaster.sh
@@ -60,7 +60,6 @@ cp -r /tmp/remaster/usr/share/backgrounds /usr/share/
 # Clean up
 rm -rf /tmp/remaster
 sudo apt-get install -y calamares grub-efi-amd64 grub-efi-amd64-bin grub-efi-amd64-signed
-sudo apt-get install -y live-boot live-boot-doc live-config live-config-doc live-config-systemd live-config-systemd live-tools live-task-localisation live-task-recommended calamares-settings-debian
 EOF
 chmod +x config/hooks/live/99-custom-script.chroot
 
