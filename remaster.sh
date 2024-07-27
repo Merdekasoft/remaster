308526fc0eff717e4d4aa427abdfbc8fe9d9de2c
Merdekasoft <merdekasoft@gmail.com>
2024-07-27 13:21:29 +0700

    Update remaster.sh

--- remaster.sh
+++ remaster.sh
@@ -11,6 +11,13 @@ cd live-build-project
 # Configure live-build with Debian Installer
 lb config --distribution bookworm --debian-installer live
 
+# Modify sources.list to include non-free repository
+mkdir -p config/archives
+cat <<EOF > config/archives/my-sources.list.chroot
+deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+EOF
+
 # Create the package list
 mkdir -p config/package-lists
 cat <<EOF > config/package-lists/my.list.chroot
@@ -34,17 +41,13 @@ laptop-mode-tools
 network-manager
 network-manager-gnome
 grub-pc
+firmware-misc-nonfree
 EOF
 
 # Create and set up the custom script hook
 mkdir -p config/hooks/live
 cat <<EOF > config/hooks/live/99-custom-script.chroot
 #!/bin/sh
-
-# Download the firmware-misc-nonfree package
-wget http://ftp.us.debian.org/debian/pool/non-free-firmware/f/firmware-nonfree/firmware-misc-nonfree_20230210-5_all.deb
-dpkg -i firmware-misc-nonfree_20230210-5_all.deb
-rm firmware-misc-nonfree_20230210-5_all.deb
 
 # Clone the repository
 git clone https://github.com/Merdekasoft/remaster.git /tmp/remaster
@@ -54,6 +57,12 @@ cp -r /tmp/remaster/etc/skel /etc/
 
 # Copy usr/share/backgrounds to /usr/share/backgrounds
 cp -r /tmp/remaster/usr/share/backgrounds /usr/share/
+
+# Add GRUB_DISABLE_OS_PROBER=false to GRUB config
+echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
+
+# Update GRUB configuration
+update-grub
 
 # Clean up
 rm -rf /tmp/remaster
